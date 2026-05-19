27ad13ab098ed7bb41097849cbec9421596bfb0b
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-19 20:25:42 -0300

    Final do desafio

--- src/br/com/alura/minhasmusicas/principal/Principal.java
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.minhasmusicas.principal;
 
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
 import br.com.alura.minhasmusicas.modelos.Musica;
 import br.com.alura.minhasmusicas.modelos.Podcast;
 
@@ -29,5 +30,12 @@ public class Principal {
         for (int i = 0; i < 10000; i++) {
             meuPodcast.curte();
         }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+
+
     }
 }
